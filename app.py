01e067f277af15e4e85b5cc8ecdfffcb80348c86
crashbandicootsthe1 <144353514+crashbandicootsthe1@users.noreply.github.com>
2023-10-12 09:35:14 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import subprocess
+import os
 
 app = Flask(__name)
 
+# Set the database URL as an environment variable
+os.environ["DATABASE_URL"] = "https://crashbandicootsthe1.github.io/teams/"
+
 @app.route('/run-python-script', methods=['POST'])
 def run_python_script():
-try:
-    output = subprocess.check_output(["python", "clans.py"], stderr=subprocess.STDOUT, text=True)
-    return jsonify({"output": output})
-except subprocess.CalledProcessError as e:
-    app.logger.error(f"Error running 'clans.py': {str(e.output)}")
-    return jsonify({"error": str(e.output)})
+    try:
+        # Access the database URL from the environment variable
+        database_url = os.environ.get("DATABASE_URL", "")
+        
+        # You can use 'database_url' in your 'clans.py' script as needed
+        # For example:
+        subprocess.check_output(["python", "clans.py", database_url], stderr=subprocess.STDOUT, text=True)
+
+        output = f"Running 'clans.py' with DATABASE_URL={database_url}"
+
+        return jsonify({"output": output})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
 if __name__ == '__main__':
     app.run()
