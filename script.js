ed4da6419297e3dd7ece289e96244d8cf01a4e75
crashbandicootsthe1 <144353514+crashbandicootsthe1@users.noreply.github.com>
2023-10-12 08:19:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const outputDiv = document.getElementById("output-div");
 
     runScriptButton.addEventListener("click", async function () {
-        // Make an HTTP request to the API endpoint that triggers the script execution
-        const response = await fetch("/api/execute-script");
+        // Make an HTTP request to a server to run the Python script
+        try {
+            const response = await fetch("/run-python-script", {
+                method: "POST"
+            });
 
-        if (response.ok) {
-            const result = await response.json();
-            outputDiv.textContent = JSON.stringify(result, null, 2);
-        } else {
-            outputDiv.textContent = "Error: Failed to run the script.";
+            if (response.ok) {
+                const result = await response.json();
+                outputDiv.textContent = JSON.stringify(result, null, 2);
+            } else {
+                outputDiv.textContent = "Error: Failed to run the script.";
+            }
+        } catch (error) {
+            outputDiv.textContent = "Error: " + error.message;
         }
     });
 });
