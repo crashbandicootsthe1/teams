4ea50f7233538d57f0339acc2b9141d127bf9b1f
crashbandicootsthe1 <144353514+crashbandicootsthe1@users.noreply.github.com>
2023-10-12 08:23:21 +0400

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,11 @@ app = Flask(__name)
 
 @app.route('/run-python-script', methods=['POST'])
 def run_python_script():
-    try:
-        # Run "clans.py" and capture its output
-        output = subprocess.check_output(["python", "clans.py"], stderr=subprocess.STDOUT, text=True)
-        return jsonify({"output": output})
-    except subprocess.CalledProcessError as e:
-        return jsonify({"error": str(e.output)})
-
+try:
+    output = subprocess.check_output(["python", "clans.py"], stderr=subprocess.STDOUT, text=True)
+    return jsonify({"output": output})
+except subprocess.CalledProcessError as e:
+    app.logger.error(f"Error running 'clans.py': {str(e.output)}")
+    return jsonify({"error": str(e.output)})
 if __name__ == '__main__':
     app.run()
